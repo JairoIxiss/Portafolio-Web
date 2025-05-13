a5746dc662511c835b32d35c4cc2bb154833b09b
JairoIxiss <jairoixis7@gmail.com>
2025-05-13 14:58:38 -0600

    animaciones sobre mi

---
+++ js/fadeUp.js
@@ -0,0 +1,15 @@
+ document.addEventListener("DOMContentLoaded", function () {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('fade-up');
+          observer.unobserve(entry.target);
+        }
+      });
+    }, {
+      threshold: 0.2 // Se activa cuando el 20% del elemento esté visible
+    });
+
+    const elementosAnimar = document.querySelectorAll('.img-perfil, .presente, .descripcion');
+    elementosAnimar.forEach(el => observer.observe(el));
+  });
